5db6c3b0a46672063ecb0e6fcb92ec7228f6bcc0
Samb <samb@townsware.com>
2022-11-13 19:06:41 -0500

    SB - Update check_email form # 3

--- strong_passwords/views.py
+++ strong_passwords/views.py
@@ -10,7 +10,9 @@ FHSU - Fall 2022
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import CheckEmail
+from .functions import *
 from django.contrib import messages
+import datetime
 
 
 def home(request):
@@ -26,6 +28,20 @@ def check_email(request):
         if form.is_valid():
             # https://docs.djangoproject.com/en/4.1/ref/contrib/messages/
             messages.info(request, '"%s" is the received form input' % form.cleaned_data['check_email'])
+            check_result = check_email_function(form.cleaned_data['check_email'])
+            messages.info(request, 'Check result:')
+            if type(check_result) == list:
+                for result in check_result:
+                    for key in result.keys():
+                        # Convert Date fields from UTC to current time zone
+                        try:
+                            myout1 = str(key).ljust(12) + str(datetime.datetime.strptime(str(result[key]), '%Y-%m-%dT%H:%M:%SZ'))
+                        except:
+                            myout1 = str(key).ljust(12) + str(result[key])
+                        messages.info(request, myout1)
+            else:
+                myout1 = str(check_result)
+            messages.info(request, myout1)
             # redirect to a new URL:
             return HttpResponseRedirect('/check_email/')
         messages.success(request, 'Unsuccessful registration. Invalid information.')
