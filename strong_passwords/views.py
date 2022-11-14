ed51b21639bbeb75dc03ba58141692ad2f4d392e
Samb <samb@townsware.com>
2022-11-14 14:16:05 -0500

    SB - Update check_email form # 4

--- strong_passwords/views.py
+++ strong_passwords/views.py
@@ -13,6 +13,12 @@ from .forms import CheckEmail
 from .functions import *
 from django.contrib import messages
 import datetime
+from django.template.defaulttags import register
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
 
 
 def home(request):
@@ -26,25 +32,17 @@ def check_email(request):
         form = CheckEmail(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            # https://docs.djangoproject.com/en/4.1/ref/contrib/messages/
             check_result = check_email_function(form.cleaned_data['check_email'])
-#            messages.info(request, '"%s" is the received form input' % form.cleaned_data['check_email'])
-#            messages.info(request, 'Check result:')
-            if type(check_result) == list:
-                for result in check_result:
-                    for key in result.keys():
-                        # Convert Date fields from UTC to current time zone
-                        try:
-                            myout1 = str(key).ljust(12) + str(datetime.datetime.strptime(str(result[key]), '%Y-%m-%dT%H:%M:%SZ'))
-                        except:
-                            myout1 = str(key).ljust(12) + str(result[key])
-                        messages.info(request, myout1)
-            else:
-                myout1 = '"' + str(form.cleaned_data['check_email']) + '" ' + str(check_result)
-            messages.info(request, myout1)
-            # redirect to a new URL:
-            return HttpResponseRedirect('/check_email/')
-        messages.success(request, 'Unsuccessful registration. Invalid information.')
+            keys = set()
+            for item in check_result:
+                keys.update(set(item))
+            key_order = sorted(keys)
+            context = {
+                'check_result': check_result,
+                'key_order': key_order,
+            }
+        return render(request, 'check_email.html', context)
+#            return HttpResponseRedirect('/check_email/')
     # if a GET (or any other method) we'll create a blank form
     else:
         form = CheckEmail()
