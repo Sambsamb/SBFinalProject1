1eb492d98f0151b3ea2c6c290ffe1ab64d67f23f
Samb <samb@townsware.com>
2022-11-13 19:06:30 -0500

    SB - Update check_email form # 3

--- strong_passwords/functions.py
+++ strong_passwords/functions.py
@@ -24,7 +24,7 @@ def check_url(url):
         raise SystemExit(f"{url}: is Not reachable \nError details: {e}")
 
 
-def check_email(email):
+def check_email_function(email):
     base_url = 'https://haveibeenpwned.com/api/v3/'
     api_call = 'pasteaccount/'
     headers = {
@@ -32,32 +32,26 @@ def check_email(email):
         'hibp-api-key': Config.api_key
     }
 
-    try:
-        response = requests.get(base_url + api_call + email, headers=headers)
-        # print(f"The response is {response.text}")
-
-        for result in response.json():
-            print()
-            print('Email'.ljust(12), email)
-            for key in result.keys():
-                # Convert Date fields from UTC to current time zone
-                try:
-                    print(str(key).ljust(12), datetime.datetime.strptime(str(result[key]), '%Y-%m-%dT%H:%M:%SZ'))
-                except:
-                    print(str(key).ljust(12), result[key])
-
-    except BaseException as e:
-        if response.status_code == 400:
-            print(f'Bad request: Bad email format "{email}"')
-        elif response.status_code == 401:
-            print(f'Unauthorized — the API key provided "{Config.api_key}" is not valid')
-        elif response.status_code == 403:
-            print('Forbidden: No user agent has been specified in the request')
-        elif response.status_code == 404:
-            print(f'Not found: The email "{email}" was not found (not pwned)')
-        elif response.status_code == 429:  # API rate exceeded
-            print(str(response.json()).split(':')[-1].replace('}', '').replace("'", '').strip())
+    response = requests.get(base_url + api_call + email, headers=headers)
+    # print(response.status_code)
+    return response.json()
 
 
-check_email('test111@domain.com')
+"""
+email = 'samb@townsware.com'
+email = 'testn.com'
+data1 = check_email_function(email)
+if type(data1) == list:
+    for result in data1:
+        print()
+        print('Email'.ljust(12), email)
+        for key in result.keys():
+            # Convert Date fields from UTC to current time zone
+            try:
+                print(str(key).ljust(12), datetime.datetime.strptime(str(result[key]), '%Y-%m-%dT%H:%M:%SZ'))
+            except:
+                print(str(key).ljust(12), result[key])
+else:
+    print(data1)
+"""
 
