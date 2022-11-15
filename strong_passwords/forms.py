970ebf4ee4306b4fc91513f6b03dd94c32d0ee9b
Samb <samb@townsware.com>
2022-11-15 07:29:17 -0500

    SB - Update check_email form and template

--- strong_passwords/forms.py
+++ strong_passwords/forms.py
@@ -11,4 +11,11 @@ from django import forms
 
 
 class CheckEmail(forms.Form):
-    check_email = forms.CharField(label='Email', max_length=100)
+    check_email = forms.EmailField(required=True,
+                                   label='Email address',
+                                   # initial='Enter email',
+                                   help_text='<small id="emailHelp" class="form-text text-muted">'
+                                             'Email is not stored. See '
+                                             '<a href ="/policy" target=_blank>Privacy Policy</a>.</small>',
+                                   max_length=100,
+                                   )
