cfc3d7fbac00e059d1387f918c95579573bb7d88
Samb <samb@townsware.com>
2022-11-16 22:27:03 -0500

    SB - Pick up API and SECRET keys from environment variables

--- SBFinalProject1/settings.py
+++ SBFinalProject1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['.azurewebsites.ne']  # ['127.0.0.1']  # ['*']  # ['.azurewebsites.net']
+ALLOWED_HOSTS = ['.azurewebsites.net']  # ['127.0.0.1']  # ['*']  # ['.azurewebsites.net']
 
 # DEBUG = True
 # ALLOWED_HOSTS = []
